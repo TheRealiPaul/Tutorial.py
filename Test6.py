1dd910aa5d0444a2993c2b29cf70e3d9947bdace
TheRealiPaul <nqlpaul@gmail.com>
2023-07-01 20:37:09 -0400

    adding new exercices

---
+++ Test6.py
@@ -0,0 +1,4 @@
+friends = ["Paul", "Martin", "Jessica", "Kassandra"]
+
+for i in range(len(friends)):
+    print(friends[i])
---
+++ Test7.py
@@ -0,0 +1,6 @@
+def cube(number):
+    return number * number * number
+
+number = float(input("Please enter a number: "))
+
+print(cube(number))

b3dad93e8347cc62b03835ed9269076603d2e54e
TheRealiPaul <nqlpaul@gmail.com>
2023-07-05 13:15:36 -0400

    Update testClone.py
    format doc

--- testClone.py
+++ testClone.py
@@ -1,14 +1,16 @@
-"""
 def clone():
-    liste = [1,2,3,4]
-    liste1 = liste * 2
-    return  liste1
+    liste = [1, 2, 3, 4]
+    liste1 = liste * 3
+    return liste1
 
-print(clone())
-"""
 
 def clone2(liste):
-    liste1 = liste + liste
-    return(liste1)
+    liste1 = liste + liste + liste
+    return liste1
 
-print(clone2([1,2,3,4,5,6]))
+
+print("clone: ", clone())
+
+print("clone2: ", clone2([1, 2, 3, 4]))
+
+# I notice that it is the same result

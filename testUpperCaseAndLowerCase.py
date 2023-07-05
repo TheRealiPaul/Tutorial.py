96cfd8fa10ded460045e469e05b8b4e0d7864bff
TheRealiPaul <nqlpaul@gmail.com>
2023-07-05 13:42:50 -0400

    Create testUpperCaseAndLowerCase.py

---
+++ testUpperCaseAndLowerCase.py
@@ -0,0 +1,10 @@
+first_name = "Paul"
+last_name = "Foo"
+
+print("My first name in upper case is: ", first_name.upper())
+
+print("My last name in upper case is: ", last_name.upper())
+
+print("My first name in lower case is: ", first_name.lower())
+
+print("My last name in lower case is: ", last_name.lower())

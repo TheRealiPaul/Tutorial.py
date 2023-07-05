97d23329b18670509be8f626d7359fc2860a0495
TheRealiPaul <nqlpaul@gmail.com>
2023-07-05 13:41:54 -0400

    Create in_operator_test.py

---
+++ in_operator_test.py
@@ -0,0 +1,10 @@
+name = "Paul"
+
+list_of_names = ["John", "Marie", "Jesus", "Joseph"]
+
+# "in" operator means it checks and iterating.
+if name in list_of_names:
+    print("Paul is in the list")
+else:
+    print("Paul is not in the list")
+
